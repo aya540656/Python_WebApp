b5be2950b9b061abe10b1e28865465e38ea1ca40
aya540656 <2104240003oa@edu.oca.ac.jp>
2023-10-07 13:44:18 +0900

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,7 @@ def index():
 @app.route('/another')
 def another_msg():
     return 'Another Response'
+
+@app.route('/test_request/<param>')
+def test_request(param):
+    return f'test_request:{param}'
